e85c89903de0855847faf8aa2161a739fcca2757
SC6 <98927145+s0984094@users.noreply.github.com>
2022-09-29 14:29:02 -0400

    Create MicrowaveTester.java

---
+++ MicrowaveTester.java
@@ -0,0 +1,15 @@
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		Microwave myMicrowave = new Microwave();
+		
+		myMicrowave.time();
+		myMicrowave.time();
+		myMicrowave.power();
+		myMicrowave.start();
+		myMicrowave.reset();
+		
+
+	}
+
+}
