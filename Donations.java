3465e1e4b39f6c16570e232b990bd193d87fbbc2
SC6 <98927145+s0984094@users.noreply.github.com>
2022-10-20 15:02:07 -0400

    Update Donations.java

--- Donations.java
+++ Donations.java
@@ -1,4 +1,4 @@
-package edu.monmouth.cs176l.donations;
+package edu.monmouth.CS176L.Donations;
 
 public class Donations {
 	private int numIndividual = 0;
@@ -41,9 +41,22 @@ public class Donations {
 		
 	}
 	public void getStatistics() {
-		System.out.printf("Individual: #:%d%n",numIndividual, " $",amtIndividual, "\n",
+		System.out.print("Individual: #:");
+		System.out.print(numIndividual);
+		System.out.print(" $");
+		System.out.println(amtIndividual);
+		System.out.print("Business: #:");
+		System.out.print(numBusiness);
+		System.out.print(" $");
+		System.out.println(amtBusiness);
+		System.out.print("Other: #:");
+		System.out.print(numOther);
+		System.out.print(" $");
+		System.out.print(amtOther);
+
+		/*System.out.printf("Individual: #:%d%n",numIndividual, " $",amtIndividual, "\n",
 				"Business: #%d%n:",numBusiness , " $",amtBusiness, "\n",
-				"Other: #:",numOther, " $", amtOther);
+				"Other: #:",numOther, " $", amtOther);*/
 		
 	}
 }
