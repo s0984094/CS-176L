75e56e8fc5519c569519c56c69a254000a71c352
SC6 <98927145+s0984094@users.noreply.github.com>
2022-12-12 14:27:08 -0500

    Update BattingStats.java

--- BattingStats.java
+++ BattingStats.java
@@ -1,79 +1,107 @@
 import java.io.IOException;
-import java.io.InvalidObjectException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import java.util.StringTokenizer;
 
 public class BattingStats {
-
-
+	
 	public static void main(String[] args) throws IOException {
-		Document battingStats2022;
+		
+		String subHeading1 = "Player Name: ";
+		String subHeading2 = "Wins Above Replacement value: ";
+
+		Document battingStats2022 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2022&month=0&season1=2022&ind=0").get();;
 		Document battingStats2021;
 		Document battingStats2020;
-		Document battingStats2020thu2022;
-		
 
+		
 /////////////////////TOP BATTING STATS FOR 2022////////////////////////////////////////
-System.out.println("Top 10 Batters for 2022 Season");
-
-		
+		ArrayList<Player> ttp2022= new ArrayList<Player>();
+		ttp2022 = new Player[10];
+	
 		battingStats2022 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2022&month=0&season1=2022&ind=0").get();
-		Elements table2022 = battingStats2022.select("table.rgMasterTable");
-		Elements tbody2022 =table2022.select("tbody") ;
-		Elements tr2022 = tbody2022.select("tr");
-		String players2022 = tr2022.select("td.grid_line_regular a").text();
-			//System.out.println(players2022);
-			
-			String[] playerArray22 = players2022.split(" ", 0);
 		
-				for(int i =0; i<playerArray22.length;i=i+3) {
-					System.out.println(playerArray22[i]+ " " + playerArray22[i+1]);
-				}
-
-	
-			
+		ttp2022 [0] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__0");
+		ttp2022 [1] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__1");
+		ttp2022 [2] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__2");
+		ttp2022 [3] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__3");
+		ttp2022 [4] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__4");
+		ttp2022 [5] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__5");
+		ttp2022 [6] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__6");
+		ttp2022 [7] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__7");
+		ttp2022 [8] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__8");
+		ttp2022 [9] = new Player(battingStats2022,"LeaderBoard1_dg1_ctl00__9");
+		
+		System.out.println("\n"+"------------------------- Top Ten MLB Batters for 2022 -------------------------");
+		System.out.printf("%20s %55s %n",subHeading1, subHeading2);
+		for(int i=0; i<ttp2022.length; i++) {
+			System.out.printf("%-20s %40s %n", ttp2022[i].getName(), ttp2022[i].getWarNum());
+		}
+		
 ///////////////////TOP BATTING STATS FOR 2021 ///////////////////////////////////////
-		System.out.println("Top 10 Batters for 2021 Season");
-			
-		String topBatters2021[];
-		topBatters2021 = new String[10];
-		
-		battingStats2021 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2021&month=0&season1=2021&ind=0").get();
-		Elements table2021 = battingStats2021.select("table.rgMasterTable");
-		Elements tbody2021 =table2021.select("tbody") ;
-		Elements tr2021 = tbody2021.select("tr");
-		String players2021 = tr2021.select("td.grid_line_regular a").text();
-			//System.out.println(players2021);
-			
-			String[] playerArray21 = players2021.split(" ", 0);
-			
-			//for loop that takes stats from the top10 Batters from the 30 players in the player array.
-			for(int i =0; i<playerArray21.length;i=i+3) {
-				System.out.println(playerArray21[i]+ " " + playerArray21[i+1]);
-			}
-
-			
-			
-///////////////////TOP BATTING STATS FOR 2020 ///////////////////////////////////////
-		String topBatters2020[];
-		topBatters2020 = new String[10];	
-			
-		battingStats2020 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2020&month=0&season1=2020&ind=0").get();
-		Elements table2020 = battingStats2020.select("table.rgMasterTable");
-		Elements tbody2020 =table2020.select("tbody") ;
-		Elements tr2020 = tbody2020.select("tr");
-		String players2020 = tr2020.select("td.grid_line_regular").text();
-			System.out.println(players2020); 
-			
-
-		
-
-		
-				
-	}
-
+		Player[] ttp2021;
+		ttp2021 = new Player[10];
 	
+		battingStats2021 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2021&month=0&season1=2021&ind=0").get();
+		
+		ttp2021 [0] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__0");
+		ttp2021 [1] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__1");
+		ttp2021 [2] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__2");
+		ttp2021 [3] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__3");
+		ttp2021 [4] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__4");
+		ttp2021 [5] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__5");
+		ttp2021 [6] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__6");
+		ttp2021 [7] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__7");
+		ttp2021 [8] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__8");
+		ttp2021 [9] = new Player(battingStats2021,"LeaderBoard1_dg1_ctl00__9");
+		
+		System.out.println("\n"+"------------------------- Top Ten MLB Batters for 2021 -------------------------");
+		System.out.printf("%20s %55s %n",subHeading1, subHeading2);
+		for(int i=0; i<ttp2021.length; i++) {
+			System.out.printf("%-20s %40s %n", ttp2021[i].getName(), ttp2021[i].getWarNum());
+		}
+
+///////////////////TOP BATTING STATS FOR 2020 ///////////////////////////////////////
+		Player[] ttp2020;
+		ttp2020 = new Player[10];
+	
+		battingStats2020 = Jsoup.connect("https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=y&type=8&season=2020&month=0&season1=2020&ind=0").get();
+		
+		ttp2020 [0] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__0");
+		ttp2020 [1] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__1");
+		ttp2020 [2] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__2");
+		ttp2020 [3] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__3");
+		ttp2020 [4] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__4");
+		ttp2020 [5] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__5");
+		ttp2020 [6] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__6");
+		ttp2020 [7] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__7");
+		ttp2020 [8] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__8");
+		ttp2020 [9] = new Player(battingStats2020,"LeaderBoard1_dg1_ctl00__9");
+		
+		System.out.println("\n"+"------------------------- Top Ten MLB Batters for 2020 -------------------------");
+		System.out.printf("%20s %55s %n",subHeading1, subHeading2);
+		for(int i=0; i<ttp2020.length; i++) {
+			System.out.printf("%-20s %40s %n", ttp2020[i].getName(), ttp2020[i].getWarNum());
+		}
+////////////////////////////TOP 10 HITTERS  2020 - 2022////////////////////////////////////////////////
+		ArrayList<Player> bestOfSeasons = new ArrayList<Player>();
+		bestOfSeasons.add; 
+		
+	
+		
+
+		
+		
+		overAllTop10 = Arrays.copyOfRange(ttp2022, 0, 10);
+		Arrays.copyOfRange(ttp2021, 0, 10);
+		
+		
+		for(int i=0; i<overAllTop10.length; i++) {
+			System.out.println(overAllTop10[i].getName());
+		}
+		
+		
+	}
 }
